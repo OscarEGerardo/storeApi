e93ccedc455fd695f2e8056ab4dee94848ec5fcf
Oscar Gerardo <oscaregerardo@gmail.com>
2016-06-17 22:35:19 -0700

    Las promociones estan funcionando correctamente

--- routes/payments.js
+++ routes/payments.js
@@ -34,7 +34,6 @@ router.post('/', validate({ body: PaymentSchema }), function (req, res) {
         return req.body.items.indexOf(item) == pos;
     });
 
-
     models.sequelize.transaction(function (t) {
         return models.sequelize.Promise.map(uniqueArray, function (code) {
             return models.Product.findOne({
@@ -46,12 +45,16 @@ router.post('/', validate({ body: PaymentSchema }), function (req, res) {
                 }],
                 transaction: t
             }).then(function (productFound) {
+                if(productFound == null) throw Error(code + ' - Not found');
                 total += getPrice(productFound.dataValues.price, counts[productFound.dataValues.code], productFound.dataValues.Promotion);
                 return;
             });
         }).then(function (params) {
             res.send({ items: req.body.items, total });
         });
+    }).catch(function (error) {
+        res.status(400);
+        res.send({statusText: error.message});
     });
 });
 
@@ -65,17 +68,23 @@ function getPrice(price, count, promotion) {
             case 'BULK':
                 return bulk(price, count, JSON.parse(promotion.dataValues.data));
             default:
-                break;
+                return price * count;
         }
     }
 }
 
 function xForX(price, count, data) {
-    return price * count;
- }
+    var acc = (Math.floor(count / data.buy) * data.pay) * price;
+    acc += (count % data.buy) * price;
+
+    return acc;
+}
 
 function bulk(price, count, data) {
-    return price * count;
- }
+    if (count >= data.buy)
+        return count * data.price;
+    else
+        return price * count;
+}
 
 module.exports = router;
